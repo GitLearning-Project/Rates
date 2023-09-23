ef0bf850b7210083650754c3fc13c2b11b151d07
Etude <content@etude.org>
2023-09-23 14:30:04 +0700

    отображение таблицы курсов всех валют по отношению к рублю

--- rates.py
+++ rates.py
@@ -8,17 +8,15 @@ response = requests.get(URL) # выполняем HTTP-запрос методо
 tree = ET.fromstring(response.text) # Преобразуем строку с XML-данными в объект
 print(tree) # выведем объект
 
-code_valute = "USD" # например, доллар
-valute_value = None # курс
+currency_rates = {}  # Словарь для хранения курсов валют
 
 for valute in tree.findall(".//Valute"):
     char_code = valute.find("CharCode").text
-    if char_code == code_valute:
-        nominal = valute.find("Nominal").text # например: 10
-        name_valute = valute.find("Name").text # например: Египетских фунтов
-        valute_value = valute.find("Value").text # 31,53... (рублей)
+    value = valute.find("Value").text
+    currency_rates[char_code] = value
 
-if valute_value:
-    print(f"{nominal} {name_valute} ({code_valute}) = {valute_value} рублей")
-else:
-    print(f"Курс {code_valute} на текущий день неизвестен")
+# Выводим таблицу с курсами всех валют
+print("Курсы валют к рублю:")
+print("--------------------")
+for char_code, value in currency_rates.items():
+    print(f"{char_code}: {value}")
