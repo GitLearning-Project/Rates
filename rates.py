465af5fb8df8d79f2d24687d82786387fe03b38e
Etude <content@etude.org>
2023-09-23 14:19:41 +0700

    добавление меню с выбором действий

--- rates.py
+++ rates.py
@@ -8,17 +8,23 @@ response = requests.get(URL) # выполняем HTTP-запрос методо
 tree = ET.fromstring(response.text) # Преобразуем строку с XML-данными в объект
 print(tree) # выведем объект
 
-code_valute = "USD" # например, доллар
-valute_value = None # курс
+user_case = int(input("Вывести информацию по коду валюты - введите 1\nВывести таблицу курсов всех валют к рублю - введите 2\n"))
 
-for valute in tree.findall(".//Valute"):
-    char_code = valute.find("CharCode").text
-    if char_code == code_valute:
-        nominal = valute.find("Nominal").text # например: 10
-        name_valute = valute.find("Name").text # например: Египетских фунтов
-        valute_value = valute.find("Value").text # 31,53... (рублей)
+if user_case == 1:
+    code_valute = "USD" # например, доллар
+    valute_value = None # курс
 
-if valute_value:
-    print(f"{nominal} {name_valute} ({code_valute}) = {valute_value} рублей")
+    for valute in tree.findall(".//Valute"):
+        char_code = valute.find("CharCode").text
+        if char_code == code_valute:
+            nominal = valute.find("Nominal").text # например: 10
+            name_valute = valute.find("Name").text # например: Египетских фунтов
+            valute_value = valute.find("Value").text # 31,53... (рублей)
+
+    if valute_value:
+        print(f"{nominal} {name_valute} ({code_valute}) = {valute_value} рублей")
+    else:
+        print(f"Курс {code_valute} на текущий день неизвестен")
 else:
-    print(f"Курс {code_valute} на текущий день неизвестен")
+    # здесь должен быть наш новый функционал, который выводит таблицу курсов всех валют
+    print("ok")
