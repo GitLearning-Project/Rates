f8fb3cf514196cdc6559b70e7356bc6ccedca3d5
Etude <content@etude.org>
2023-09-23 17:31:44 +0700

    добавление возможности вывода списка курсов всех валют в отдельный файл

--- rates.py
+++ rates.py
@@ -1,5 +1,6 @@
 import requests
 import xml.etree.ElementTree as ET
+import datetime
 
 def get_currency_data(url):
     response = requests.get(url)
@@ -22,10 +23,17 @@ def get_currency_by_code(currency_tree, code):
 
 def display_currency_info(currency_data):
     if currency_data:
-        print(f"{currency_data['nominal']}  {currency_data['name']}     ({code_valute}) = {currency_data['value']} рублей")
+        print(f"{currency_data['nominal']} {currency_data['name']} ({code_valute}) = {currency_data['value']} рублей")
     else:
         print(f"Курс {code_valute} на текущий день неизвестен")
-        
+
+def save_currency_exchange_rates(currency_rates):
+    with open("exchange_rates.txt", "a", encoding="utf-8") as file:
+        current_datetime = datetime.datetime.now()
+        file.write(f"Дата и время сохранения: {current_datetime}\n")
+        for char_code, value in currency_rates.items():
+            file.write(f"{char_code}: {value}\n")
+        file.write("--------------------------\n")
 
 def display_all_currency_rates(currency_tree):
     currency_rates = {}
@@ -39,6 +47,8 @@ def display_all_currency_rates(currency_tree):
     print("--------------------")
     for char_code, value in currency_rates.items():
         print(f"{char_code}: {value}")
+    # Сохраняем список курсов в файл exchange_rates.txt
+    save_currency_exchange_rates(currency_rates)
 
 
 URL = "https://cbr.ru/scripts/XML_daily.asp"
@@ -47,10 +57,8 @@ currency_tree = get_currency_data(URL)
 user_case = int(input("Вывести информацию по коду валюты - введите 1\nВывести таблицу курсов всех валют к рублю - введите 2\n"))
 
 if user_case == 1:
-    # Вывод курса определённой валюты
     code_valute = input("Введите код валюты: ").upper()
-    currency_data =     get_currency_by_code(currency_tree, code_valute)
+    currency_data = get_currency_by_code(currency_tree, code_valute)
     display_currency_info(currency_data)
 else:
-    # Вывод курсов всех валют по отношению к рублю
     display_all_currency_rates(currency_tree)
